9c8c4e889b28923160f052bf96516009f3d6b517
Sharad Raj <dev_sharad@outlook.com>
2021-05-13 13:09:07 +0530

    Update theme.config.js

--- src/theme.config.js
+++ src/theme.config.js
@@ -1,4 +1,4 @@
 export const SITE_TITLE = "Awesome Notes"
 export const USER_IMAGE_URL = "https://avatars2.githubusercontent.com/u/36638057?s=460&u=0cbe4fd6b5a3655d91b3eb86db834d0829a0c086&v=4"
 export const GITHUB_USERNAME = "sharadcodes"
-export const REPOSITORY = "notes"
+export const REPOSITORY = "github-issue-notes-theme"
